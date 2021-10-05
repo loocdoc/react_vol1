03579d24555a610987626e33d8b57df6dd3529d9
loocdoc <tokochagunava@yahoo.com>
2021-10-05 23:13:10 +0400

    check

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import { User } from "./components/User";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" width={400} />
-        <p>
-          Salqus - Smart Web project.
-        </p>
-        <a
-          className="App-link"
-          href="https://salqus.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Visit my Web page, which front-end part was done by me! :)
-        </a>
+    <div className="container">
+      <header className="box">
+        <User />
       </header>
     </div>
   );
---
+++ src/components/User.js
@@ -0,0 +1,9 @@
+export const User = () => {
+    return (
+        <div className="columns">
+            <div className="column">
+                <h2 className="has-text-success">User Component</h2>
+            </div>
+        </div >
+    );
+};
