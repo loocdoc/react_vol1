f151bbd95c5dde89b8174ff28555a6c061cd57b4
loocdoc <tokochagunava@yahoo.com>
2021-10-06 10:31:56 +0400

    for branch

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,20 @@
+import { formatUser } from "../helpers/user";
+
+
+const currentUser = {
+    firstName: "John",
+    lastName: "Doe",
+};
+
+
 export const User = () => {
     return (
-        <div className="columns">
-            <div className="column">
-                <h2 className="has-text-success">User Component</h2>
-            </div>
-        </div >
+        <>
+            <div className="columns">
+                <div className="column">
+                    <h2 className="has-text-success">User Component - {formatUser(currentUser)}</h2>
+                </div>
+            </div >
+        </>
     );
 };
